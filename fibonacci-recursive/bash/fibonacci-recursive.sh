5cbb1da530c56f2fb75b580b39ea351e2655f135
Michael Harry <michael@compilefailed.com>
2021-09-08 20:06:53 -0500

    Use more readable arithemetic expansion syntax

--- fibonacci-recursive/bash/fibonacci-recursive.sh
+++ fibonacci-recursive/bash/fibonacci-recursive.sh
@@ -14,17 +14,10 @@ function get_fib()
         return
     fi
 
-   local fibNumberMinus2=$1
-   let fibNumberMinus2=fibNumberMinus2-2
-   local fibNumberMinus1=$1
-   let fibNumberMinus1=fibNumberMinus1-1
+   local f1=$(get_fib $(($1-2)))
+   local f2=$(get_fib $(($1-1)))
 
-   local f1=$(get_fib $fibNumberMinus2)
-   local f2=$(get_fib $fibNumberMinus1)
-
-   local result=$f1
-   let result=result+f2
-   echo $result
+   echo $(($f1+$f2))
 }
 
 echo $(get_fib $1)
