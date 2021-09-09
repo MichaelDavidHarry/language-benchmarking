27a62cea2bf3c135d3a610873cba6962d4937713
Michael Harry <michael@compilefailed.com>
2021-09-08 20:37:03 -0500

    Remove unneeded temporary variables

--- fibonacci-recursive/sh/fibonacci-recursive.sh
+++ fibonacci-recursive/sh/fibonacci-recursive.sh
@@ -8,10 +8,7 @@ get_fib()
         return
     fi
 
-   f1=$(get_fib $(($1-2)))
-   f2=$(get_fib $(($1-1)))
-
-   echo $(($f1+$f2))
+   echo $(( $(get_fib $(($1-2))) + $(get_fib $(($1-1))) ))
 }
 
 echo $(get_fib $1)
