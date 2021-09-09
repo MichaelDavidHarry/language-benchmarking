e4e3f8aa86882d1bb8848e850e3dd034d7605337
Michael Harry <michael@compilefailed.com>
2021-09-08 20:01:04 -0500

    Use modern command substitution syntax

--- fibonacci-recursive/bash/fibonacci-recursive.sh
+++ fibonacci-recursive/bash/fibonacci-recursive.sh
@@ -19,8 +19,8 @@ function get_fib()
    local fibNumberMinus1=$1
    let fibNumberMinus1=fibNumberMinus1-1
 
-   local f1=`get_fib $fibNumberMinus2`
-   local f2=`get_fib $fibNumberMinus1`
+   local f1=$(get_fib $fibNumberMinus2)
+   local f2=$(get_fib $fibNumberMinus1)
 
    local result=$f1
    let result=result+f2
