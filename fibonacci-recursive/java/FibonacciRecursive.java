31b3e256a0de67f239b088d20d8059ca0d35f5e9
Michael Harry <michael@compilefailed.com>
2021-09-12 08:02:02 -0500

    Parse param as long in Java

--- fibonacci-recursive/java/FibonacciRecursive.java
+++ fibonacci-recursive/java/FibonacciRecursive.java
@@ -1,5 +1,10 @@
 public class FibonacciRecursive
 {
+    public static void main(String[] args)
+    {
+        System.out.println(getFib(Long.parseLong(args[0])));
+    }
+
     private static long getFib(long fibNumber)
     {
         if(fibNumber == 1 || fibNumber == 2)
@@ -7,10 +12,5 @@ public class FibonacciRecursive
             return 1;
         }
         return getFib(fibNumber - 2) + getFib(fibNumber - 1);
-    }
-
-    public static void main(String[] args)
-    {
-        System.out.println(getFib(Integer.parseInt(args[0])));
     }
 }
